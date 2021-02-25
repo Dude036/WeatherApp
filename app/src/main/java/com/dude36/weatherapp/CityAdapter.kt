97dcefcc3584a28382b96f94d171272c186c8e76
Joshua Higham <josh@higham.com>
2021-02-25 16:16:26 -0700

    Layout Inflator setup

--- app/src/main/java/com/dude36/weatherapp/CityAdapter.kt
+++ app/src/main/java/com/dude36/weatherapp/CityAdapter.kt
@@ -1,16 +1,20 @@
 package com.dude36.weatherapp
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class CityAdapter(internal var context: Context, internal var cityList: List<City>) : RecyclerView.Adapter<CityCard>()  {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CityCard {
-        TODO("Not yet implemented")
+        val layoutView = LayoutInflater.from(parent.context).inflate(R.layout.card_layout, parent, false)
+        return CityCard(layoutView)
     }
 
     override fun onBindViewHolder(holder: CityCard, position: Int) {
-        TODO("Not yet implemented")
+        holder.cityName.text = cityList[position].cityName
+        holder.cityTempF.text = cityList[position].cityTempF.toString()
+        holder.cityTempC.text = cityList[position].cityTempC.toString()
     }
 
     override fun getItemCount(): Int {
