131c92c46427b3cdadba3eaae928e2ff8233387c
Joshua Higham <josh@higham.com>
2021-02-25 16:33:03 -0700

    On Click Listener setup

--- app/src/main/java/com/dude36/weatherapp/CityAdapter.kt
+++ app/src/main/java/com/dude36/weatherapp/CityAdapter.kt
@@ -3,6 +3,7 @@ package com.dude36.weatherapp
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
 class CityAdapter(internal var context: Context, internal var cityList: List<City>) : RecyclerView.Adapter<CityCard>()  {
@@ -12,9 +13,16 @@ class CityAdapter(internal var context: Context, internal var cityList: List<Cit
     }
 
     override fun onBindViewHolder(holder: CityCard, position: Int) {
+        // Update Data
         holder.cityName.text = cityList[position].cityName
         holder.cityTempF.text = cityList[position].cityTempF.toString()
         holder.cityTempC.text = cityList[position].cityTempC.toString()
+
+        // Set OnClickListener
+        holder.layout.setOnClickListener {
+            // TODO (Josh): Setup Fragment for Specific City
+            Toast.makeText(context, holder.cityName.text as String? + " fragment here", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun getItemCount(): Int {
