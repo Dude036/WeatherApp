766508b53985f14701c0939d2ec10076d4013d3a
Joshua Higham <josh@higham.com>
2021-02-25 15:03:09 -0700

    Adding Base Project

---
+++ app/src/main/java/com/dude36/weatherapp/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.dude36.weatherapp
+
+import android.media.Image
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+
+class City(var icon: Image, var cityName: String, var cityTempF: Float, var cityTempC: Float)
+
+class MainActivity : AppCompatActivity() {
+    internal var RecyclerView: RecyclerView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Setup View
+        RecyclerView = findViewById<RecyclerView>(R.id.Recycler)
+
+        // Add Hard Coded cities
+
+        // Send for Data
+
+        // New City Functionality to be added here (empty cardView Object
+    }
+}
