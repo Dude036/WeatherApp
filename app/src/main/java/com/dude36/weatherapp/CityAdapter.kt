cb7099977967c0da6a894475faa8d869c28726ee
Joshua Higham <josh@higham.com>
2021-02-25 15:06:28 -0700

    Necessary Members added

--- app/src/main/java/com/dude36/weatherapp/CityAdapter.kt
+++ app/src/main/java/com/dude36/weatherapp/CityAdapter.kt
@@ -1,6 +1,20 @@
 package com.dude36.weatherapp
 
 import android.content.Context
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 
-class CityAdapter(internal var context: Context, internal var cityList: List<City>) {
+class CityAdapter(internal var context: Context, internal var cityList: List<City>) : RecyclerView.Adapter<CityCard>()  {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CityCard {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: CityCard, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
 }
