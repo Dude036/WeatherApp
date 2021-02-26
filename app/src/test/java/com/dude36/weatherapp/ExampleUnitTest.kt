ac66409459b98626da5d9be8736444a8968667d8
Joshua Higham <josh@higham.com>
2021-02-26 16:29:22 -0700

    Building Testing Procedure

--- app/src/test/java/com/dude36/weatherapp/ExampleUnitTest.kt
+++ app/src/test/java/com/dude36/weatherapp/ExampleUnitTest.kt
@@ -1,8 +1,12 @@
 package com.dude36.weatherapp
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import org.junit.Test
 
 import org.junit.Assert.*
+import java.io.File
+import java.nio.file.Paths
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +14,24 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+//    @Test
+//    fun UrlifyTest() {
+//        MainActivity.urlifyCity()
+//    }
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun checkIconDownload() {
+        val main = MainActivity()
+        var tempCity = City(null, null, null, null)
+        main.getIcon(tempCity, "10d")
+
+        val path = Paths.get("").toAbsolutePath().toString() + "/src/main/res/drawable/image.png"
+        val fileTest = File(path)
+        assertTrue(fileTest.exists())
+
+        val image: Bitmap = BitmapFactory.decodeFile(fileTest.absolutePath)
+        while (tempCity.icon === null) {
+            Thread.sleep(1000)
+        }
     }
 }
